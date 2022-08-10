8e550ba756d6e1995fc321d40fe29cdd1913b1c7
chris frey <chris.frey@live.com>
2022-08-10 11:13:05 -0700

    practice updates

---
+++ small_problems/easy_1/list_of_digits.rb
@@ -0,0 +1,4 @@
+def digit_list(num)
+p num.to_s.split(//)
+end
+digit_list(2345234)
---
+++ small_problems/easy_1/odd.rb
@@ -0,0 +1,9 @@
+def is_odd(num)
+  if num.abs.odd?
+    puts true
+  else 
+    puts false
+  end
+end
+
+is_odd(-3)
