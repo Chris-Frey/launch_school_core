56ac2ad15475af1af89fe71f77c20351b7ed5c5c
chris frey <chris.frey@live.com>
2022-09-14 10:08:50 -0700

    12 progress

--- rb_101/lesson_6/testing.rb
+++ rb_101/lesson_6/testing.rb
@@ -1,32 +1,52 @@
-# arr = ['yes', 'no', 'maybe']
-# answer = ''
 
-# arr.each_with_index {|word, index| answer << word[index]}
+# CLUBS = "\u2663"
+# HEARTS = "\u2665"
+# DIAMONDS = "\u2666"
+# SPADES = "\u2660"
+def prompt(message)
+  puts("=> #{message}")
+end
+system 'clear'
+# prompt("Welcome to the the game of 21!")
+# sleep 2
+# prompt("Here are the rules - you'll only see them once so pay attention!:")
+# sleep 4
+# prompt("We begin the game with a normal 52-card deck.")
+# sleep 2.5
+# prompt("Try to get as close to 21 as possible!")
+# sleep 2.5
+# prompt("You start with one card face up, and one card face down that the dealer cannot see.")
+# sleep 4
+# prompt("The dealer starts with one card face up, and one card face down that you cannot see.")
+# sleep 4
+# prompt("You will go first.")
+# sleep 2
+# prompt("You may 'hit', or 'stay' after each card. Once you stay, your turn is over and the dealer goes.")
+# sleep 4.5
+# prompt("Highest score without going 'bust' (over 21 points) wins!")
+# sleep 2
+# prompt("Scoring:")
+# sleep 3
+# prompt("  cards 2 - 10 are scored at face value")
+# sleep 3
+# prompt("  Royal cards are all scored at 10 each")
+# sleep 3
+# prompt("  Aces are worth either 1 or 11, your choice depending on the situation")
+# sleep 4
+prompt("Enter any key when ready!")
+start = gets.chomp
 
-# p arr
-
-# def nth_char(array)
-#   results = ''
-#   array.each_with_index do |word, ind|
-#     results << word[ind]
-#   end
-#   results 
-# end
-# p nth_char(['yoda', 'best', 'has']) == 'yes'
-# p nth_char([]) == ''
-# p nth_char(['X-ray']) == 'X'
-# p nth_char(['No', 'No']) == 'No'
-# p nth_char(['Chad', 'Morocco', 'India', 'Algeria', 'Botswana', 'Bahamas', 'Ecuador', 'Micronesia']) ==  'Codewars'
-##########################
-
+loop do
 p_hand = []
+d_hand = []
 current_deck = []
-def deck
-  suits = ["hearts", "spades", "diamonds", "clubs"]
+
+def deck #creates a hash with arrays of hash values
+  suits = ["\u2665", "\u2660", "\u2666", "\u2663"]
   cards = (2..10).to_a, ["Jack", "King", "Queen", "Ace"] 
   full_deck = {}
-  suits.map do |suit|
-    full_deck.store(suit.to_sym, cards.flatten )
+  suits.each do |suit|
+    full_deck.store(suit, cards.flatten )
   end
   full_deck
 end
@@ -37,6 +57,10 @@ def card_points(hand)
    hand
   points = point_calculation(hand).map {|x| point_values[x]}
   points.sum
+  if points.include?(11) && points.sum > 21
+    return points.sum - 11
+  end
+
 end
 # p card_points
 
@@ -59,10 +83,8 @@ def point_calculation(hand)
   end
 end
 
-
-current_deck = []
 def game_deck
-  current_deck = deck
+  current_deck = deck.dup
 end
 # p game_deck
 
@@ -73,11 +95,4 @@ def draw
   card.store(card_suit, card_value.sample)
   card
 end
-
-def player_hand(hand)
-  hand << draw
-  hand << draw
 end
- p player_hand(p_hand)
- p point_calculation(p_hand)
- p card_points(p_hand)
--- rb_101/lesson_6/twentyone.rb
+++ rb_101/lesson_6/twentyone.rb
@@ -1,35 +1,48 @@
-# 1. Initialize deck **
-# 2. Deal cards to player and dealer
-# 3. Player turn: hit or stay
-#   - repeat until bust or "stay"
-# 4. If player bust, dealer wins.
-# 5. Dealer turn: hit or stay
-#   - repeat until total >= 17
-# 6. If dealer bust, player wins.
-# 7. Compare cards and declare winner.
 
-# What I need:
-# deck of cards
-# card points
-# player hand
-# dealer hand
-# store cards in the hands
-# draw method
-# show only 1 card publicly
-# method on whether to hit or stay for dealer
-# method to show the current cards on table
-
-
-
-p_hand = []
-d_hand = []
-current_deck = []
+# CLUBS = "\u2663"
+# HEARTS = "\u2665"
+# DIAMONDS = "\u2666"
+# SPADES = "\u2660"
 def prompt(message)
   puts("=> #{message}")
 end
+system 'clear'
+# prompt("Welcome to the the game of 21!")
+# sleep 2
+# prompt("Here are the rules - you'll only see them once so pay attention!:")
+# sleep 4
+# prompt("We begin the game with a normal 52-card deck.")
+# sleep 2.5
+# prompt("Try to get as close to 21 as possible!")
+# sleep 2.5
+# prompt("You start with one card face up, and one card face down that the dealer cannot see.")
+# sleep 4
+# prompt("The dealer starts with one card face up, and one card face down that you cannot see.")
+# sleep 4
+# prompt("You will go first.")
+# sleep 2
+# prompt("You may 'hit', or 'stay' after each card. Once you stay, your turn is over and the dealer goes.")
+# sleep 4.5
+# prompt("Highest score without going 'bust' (over 21 points) wins!")
+# sleep 2
+# prompt("Scoring:")
+# sleep 3
+# prompt("  cards 2 - 10 are scored at face value")
+# sleep 3
+# prompt("  Royal cards are all scored at 10 each")
+# sleep 3
+# prompt("  Aces are worth either 1 or 11, your choice depending on the situation")
+# sleep 4
+prompt("Enter any key when ready!")
+start = gets.chomp
 
-def deck #hash with arrays of values
-  suits = ["hearts", "spades", "diamonds", "clubs"]
+loop do
+p_hand = []
+d_hand = []
+current_deck = []
+
+def deck #creates a hash with arrays of hash values
+  suits = ["\u2665", "\u2660", "\u2666", "\u2663"]
   cards = (2..10).to_a, ["Jack", "King", "Queen", "Ace"] 
   full_deck = {}
   suits.each do |suit|
@@ -44,6 +57,11 @@ def card_points(hand)
    hand
   points = point_calculation(hand).map {|x| point_values[x]}
   points.sum
+  if points.include?(11) && points.sum > 21
+    return points.sum - 10
+  else points.sum
+  end
+
 end
 # p card_points
 
@@ -79,114 +97,152 @@ def draw
   card
 end
 
-
-
   def player_draw(hand)  
-      if hand.include?(draw)
-        then draw
-      else
       hand << draw
-      end
   end
   # player_draw(p_hand)
 
   def dealer_draw(hand)
-    if hand.include?(draw)
-      then draw
-    else
     hand << draw
-    end
   end
   # dealer_draw(p_hand)
 
   def show_player_hand(hand)
     suit = hand.map {|x| x.keys}.flatten
-    card = hand.map {|x| x.values}.flatten
-    hand.map {|x| card + suit}
-  puts  "your hand is currently: #{hand}, worth #{card_points(hand)} points"
+    number = hand.map {|x| x.values}.flatten
+    puts "Your hand, worth #{card_points(hand)} points, is:" 
+    counter = 0
+    loop do
+      puts "#{suit[counter]}#{number[counter]}"
+      break if counter == hand.length
+      counter += 1
+    end
   end
   # show_player_hand(p_hand)
 
   def show_dealer_hand(hand)
     suit = hand.map {|x| x.keys}.flatten
-    card = hand.map {|x| x.values}.flatten
-    hand.map {|x| card + suit}
-  prompt("Dealer's hand shows: #{hand[0]}, worth #{card_points(hand)} points")
+    number = hand.map {|x| x.values}.flatten
+    puts "Dealer's hand, worth #{card_points(hand)} points, is:"
+    counter = 0
+    loop do
+     puts "#{suit[counter]}#{number[counter]}"
+      break if counter == hand.length
+      counter += 1
+    end
   end
   # show_player_hand(p_hand)
 
-  # def show_dealer_hand
-  #   p "The dealer has a #{card} of #{suit} visible, worth #{d_points} points."
-  # end
-
-  
 def player_turn(hand)
   loop do
-    check_for_21(hand)
-    if check_for_bust(hand) == true
-      prompt("BUST!")
-      break
-    end
+    show_player_hand(hand)
+    sleep 2
+    break if check_for_21(hand)
+    break if check_for_bust(hand)
     prompt("Would you like to hit, or stay? 1 for hit, 2 for stay.")
     choice = gets.chomp.to_i
-      if choice == 2
-        break
-
-        elsif choice == 1
-          player_draw(hand)
-          show_player_hand(hand)
-                    
-        else
-          prompt("Please follow directions")
+    
+     if choice == 2
+      prompt("You choose to stay with #{card_points(hand)} points.")
+      sleep 2
+      system 'clear'
+      return 2
+      break
+      elsif choice == 1
+        system 'clear'
+        player_draw(hand)           
+      else
+        prompt("Please follow directions...")
+        sleep 0.5
       end
   end
 end
 
 def dealer_turn(hand)
   loop do
-    check_for_21(hand)
-    check_for_bust(hand)
-      if card_points(hand) > 17
-        prompt("Dealer holds at #{card_points(hand)}.")
-        break
-      else 
-        prompt("Dealer hits")
-          dealer_draw(hand)
-          show_dealer_hand(hand)
-      end
+    show_dealer_hand(hand)
+    sleep 2
+    break if check_for_21(hand)
+    break if check_for_bust(hand)
+    if card_points(hand) > 17
+      puts "Dealer holds at #{card_points(hand)} points."
+      sleep 4
+      break
+    else 
+      prompt("Dealer hits")
+      dealer_draw(hand)
+      
+    end
   end
 end
 
 def check_for_bust(hand)
   if card_points(hand) > 21
+    system 'clear'
     true
   end
 end
 
 def check_for_21(hand)
-  if hand == 21
+  if card_points(hand) == 21
     prompt("We have a winner!")
+    true
   end
 end
 
 #MAIN GAME FLOW
+
+  system 'clear'
   player_draw(p_hand)
   player_draw(p_hand)
-  show_player_hand(p_hand)
   dealer_draw(d_hand)
   show_dealer_hand(d_hand)
-  player_turn(p_hand)
+    sleep 1
+  dealer_draw(d_hand)
+    sleep 1
 
-  dealer_turn(d_hand)
+  if player_turn(p_hand) == 2
+    dealer_turn(d_hand)
 
-  # def who_won?()
+    prompt("Player has #{card_points(p_hand)} and
+    Dealer had #{card_points(d_hand)}")
+
+    if card_points(d_hand) == card_points(p_hand) 
+      prompt("It's a tie!")
+
+    elsif card_points(d_hand) > card_points(p_hand) && card_points(d_hand) < 22
+      prompt("Dealer won!")
+
+    else prompt("Player won!")
+    end
+
+    prompt("Again? y/n")
+    input = gets.chomp
+    if input == 'y'.downcase
+      prompt("Here we go!")
+      sleep 1
+      else 
+        prompt("thanks for playing!")
+        break
+    end
+
   
-  # end
-
-#  p "x won!
-#   play again? (y/n)"
-#   again? = gets.chomp
-#   break unless again? == 'y'
-# end
-
-
+    elsif check_for_bust(p_hand) == true
+    prompt("Oh no, bust!! Try again? y/n")
+    answer = gets.chomp
+    if answer != 'y'.downcase
+      puts "I'll be here when you return. Age is of no importance unless you're a cheese."
+     break
+    else
+    end      
+  
+    elsif check_for_21(p_hand) == true
+    prompt("Nice job! did you learn from James Bond?! Again? y/n")
+    answer = gets.chomp
+    if answer != 'y'.downcase
+      puts "Quitting while you're ahead. Good move!"
+    break
+    else
+    end
+  end
+end
